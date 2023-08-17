8d8e848a465bbff29e2752d6879d23e8e36c5ae4
Func <Funcer@outlook.com>
2023-08-17 15:11:15 +0800

    populateUserTable: Sync from the StubUserWikiAuth extension and simplify
    Simplified since the min supported version has been bumped to 1.39
    
    Bug: T324923
    Change-Id: If4729ce8824c0e0909ce87898dd0b5028a35cfef

--- populateUserTable.php
+++ populateUserTable.php
@@ -7,59 +7,13 @@
  * @file
  * @ingroup Maintenance
  * @author Jesús Martínez <martineznovo@gmail.com>
- * @version 1.0.1
- * @date 2 April 2019
+ * @version 1.3.0
+ * @date 17 August 2023
  */
 
 require_once __DIR__ . '/../maintenance/Maintenance.php';
 
 class PopulateUserTable extends Maintenance {
-
-	/**
-	 * API limits to use instead of max
-	 *
-	 * @var int
-	 */
-	protected $apiLimits;
-
-	/**
-	 * List of tables to populate users from
-	 *
-	 * @var array
-	 */
-	protected $tables;
-
-	/**
-	 * Array of valid tables and columns that contain user id and name
-	 *
-	 * @var array
-	 */
-	protected $validTables = [
-			'revision' => [
-				'id' => 'rev_user',
-				'name' => 'rev_user_text'
-			],
-			'logging' => [
-				'id' => 'log_user',
-				'name' => 'log_user_text'
-			],
-			'image' => [
-				'id' => 'img_user',
-				'name' => 'img_user_text'
-			],
-			'oldimage' => [
-				'id' => 'oi_user',
-				'name' => 'oi_user_text'
-			],
-			'filearchive' => [
-				'id' => 'fa_user',
-				'name' => 'fa_user_text'
-			],
-			'archive' => [
-				'id' => 'ar_user',
-				'name' => 'ar_user_text'
-			]
-		];
 
 	/**
 	 * Handle to the database connection
@@ -68,84 +22,72 @@ class PopulateUserTable extends Maintenance {
 	 */
 	protected $dbw;
 
-	/**
-	 * MediaWikiBot instance
-	 *
-	 * @var MediaWikiBot
-	 */
-	protected $bot;
-
 	public function __construct() {
 		parent::__construct();
 		$this->mDescription = 'Populates the user table creating stub users (user ID and name) from other tables.';
 		$this->addOption( 'db', 'Database name, if we don\'t want to write to $wgDBname', false /* required? */, true /* withArg */ );
-		$this->addOption( 'tables', 'Tables to grab users from (pipe separated list) revision, logging, image, oldimage, filearchive, archive', false, true );
 	}
 
 	public function execute() {
 		global $wgDBname;
 
-		# Get a single DB_MASTER connection
-		$this->dbw = wfGetDB( DB_MASTER, [], $this->getOption( 'db', $wgDBname ) );
+		# Get a single DB_PRIMARY connection
+		$this->dbw = wfGetDB( DB_PRIMARY, [], $this->getOption( 'db', $wgDBname ) );
 
-		$tables = $this->getOption( 'tables' );
-		if ( !is_null( $tables ) ) {
-			$this->tables = explode( '|', $tables );
-
-			# Check that no invalid tables were provided
-			$invalidTables = array_diff( $this->tables, array_keys( $this->validTables ) );
-			if ( count( $invalidTables ) > 0 ) {
-				$this->fatalError( sprintf( 'Invalid tables provided: %s',
-					implode( ',', $invalidTables ) ) );
-			}
-		} else {
-			$this->tables = array_keys( $this->validTables );
-		}
-
-		foreach ( $this->tables as $table ) {
-			$this->populateUsersFromTable( $table );
-		}
+		$this->populateUsersFromTable( 'actor', [
+			'id' => 'actor_user',
+			'name' => 'actor_name'
+		] );
 	}
 
-	function populateUsersFromTable( $table ) {
-		$columnInfo = $this->validTables[$table];
+	function populateUsersFromTable( $table, $columnInfo ) {
 		$userIDField = $columnInfo['id'];
 		$userNameField = $columnInfo['name'];
 
+		# Dummy values for all required fields
+		$e = [
+			'user_id' => '',
+			'user_name' => '',
+			'user_real_name' => '',
+			'user_password' => '',
+			'user_newpassword' => '',
+			'user_email' => '',
+			'user_touched' => wfTimestampNow(),
+			'user_token' => ''
+		];
+
 		$this->output( "Populating users from table $table..." );
-		$result = $this->dbw->select(
-			[ $table, 'user' ],
-			[ $userIDField, $userNameField ],
-			[
-				'user_id' => null,
-				"$userIDField > 0"
-			],
-			__METHOD__,
-			[
-				'DISTINCT',
-				'ORDER BY' => $userIDField
-			],
-			[
-				'user' => [ 'LEFT JOIN', "user_id=$userIDField" ]
-			]
-		);
+
 		$count = 0;
-		if ( $result ) {
-			# Dummy values for all required fields
-			$e = [
-				'user_id' => '',
-				'user_name' => '',
-				'user_real_name' => '',
-				'user_password' => '',
-				'user_newpassword' => '',
-				'user_email' => '',
-				'user_touched' => wfTimestampNow(),
-				'user_token' => ''
-			];
+		$lastUserId = 0;
+
+		while ( true ) {
+			$result = $this->dbw->select(
+				[ $table, 'user' ],
+				[ $userIDField, $userNameField ],
+				[
+					'user_id' => null,
+					"$userIDField > $lastUserId"
+				],
+				__FUNCTION__,
+				[
+					'DISTINCT',
+					'ORDER BY' => $userIDField,
+					'LIMIT' => $this->getBatchSize(),
+				],
+				[
+					'user' => [ 'LEFT JOIN', "user_id=$userIDField" ]
+				]
+			);
+
+			if ( !$result->numRows() ) {
+				break;
+			}
 
 			$row = $result->fetchRow();
 			while ( $row ) {
-				$e['user_id'] = $row[$userIDField];
+				$lastUserId = $row[$userIDField];
+				$e['user_id'] = $lastUserId;
 				$e['user_name'] = $row[$userNameField];
 				$inserted = $this->dbw->insert(
 					'user',
@@ -168,5 +110,5 @@ class PopulateUserTable extends Maintenance {
 	}
 }
 
-$maintClass = 'PopulateUserTable';
+$maintClass = PopulateUserTable::class;
 require_once RUN_MAINTENANCE_IF_MAIN;
